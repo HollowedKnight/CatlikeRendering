8f0a13af0eaebff9d4cfe6ac927af6b1f3fec03b
Philipp Bornstedt <philipp.bornstedt@innogames.com>
2017-09-01 13:01:04 +0200

    Refactored all Transformations to work with transformation matrices

--- Assets/Scripts/PositionTransformation.cs
+++ Assets/Scripts/PositionTransformation.cs
@@ -5,10 +5,18 @@ namespace DefaultNamespace
     public class PositionTransformation : Transformation
     {
         public Vector3 Position;
-        
-        public override Vector3 Apply(Vector3 point)
+
+        public override Matrix4x4 Matrix
         {
-            return Position + point;
+            get
+            {
+                Matrix4x4 matrix = new Matrix4x4();
+                matrix.SetRow(0, new Vector4(1f, 0f, 0f, Position.x));
+                matrix.SetRow(1, new Vector4(0f, 1f, 0f, Position.y));
+                matrix.SetRow(2, new Vector4(0f, 0f, 1f, Position.z));
+                matrix.SetRow(3, new Vector4(0f, 0f, 0f, 1f));
+                return matrix;
+            }
         }
     }
 }
--- Assets/Scripts/ScaleTransformation.cs
+++ Assets/Scripts/ScaleTransformation.cs
@@ -4,14 +4,19 @@ namespace DefaultNamespace
 {
     public class ScaleTransformation : Transformation
     {
-        public Vector3 scale;
-        
-        public override Vector3 Apply(Vector3 point)
+        public Vector3 Scale;
+
+        public override Matrix4x4 Matrix
         {
-            point.x *= scale.x;
-            point.y *= scale.y;
-            point.z *= scale.z;
-            return point;
+            get
+            {
+                Matrix4x4 matrix = new Matrix4x4();
+                matrix.SetRow(0, new Vector4(Scale.x, 0f, 0f, 0f));
+                matrix.SetRow(1, new Vector4(0f, Scale.y, 0f, 0f));
+                matrix.SetRow(2, new Vector4(0f, 0f, Scale.z, 0f));
+                matrix.SetRow(3, new Vector4(0f, 0f, 0f, 1f));
+                return matrix;
+            }
         }
     }
-} 
+}
